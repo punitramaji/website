05dedaf10f5b0b11294bbaa5555d7f2040a438be
Punit Ramaji <punitramaji@users.noreply.github.com>
2021-05-02 13:39:32 +0530

    update zone id of route 53 zone for cert validation

--- main.tf
+++ main.tf
@@ -95,7 +95,7 @@ resource "aws_route53_record" "certvalidation" {
   records         = [each.value.record]
   ttl             = 60
   type            = each.value.type
-  zone_id         = "???"
+  zone_id         = data.route53_zone.domain.zone_id
 }
 
 resource "aws_acm_certificate_validation" "certvalidation" {
